adfb5a0522cea21bd2f9288f8501620c280df38a
Akash Gohel <akash.gohel.37231@gmail.com>
2019-09-23 11:42:12 +0530

    Fundamantal of Hooks and Css

---
+++ src/NavBar.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import { Link } from "@reach/router";
+import { css,keyframes } from "@emotion/core";
+import colors from './colors';
+
+const spin = keyframes `
+    to{
+        transform : rotate(360deg);
+    }
+`;
+
+const NavBar = () => {
+
+    const [padding] = useState(15);
+
+   return(
+        <header
+            // onClick={() => setPadding(padding + 15)}
+            css = {css`
+                background-color: ${colors.primary}; 
+                padding:${padding}px;
+            `}
+        >
+            <Link to ="/">Adopet Me!</Link>
+            <span 
+                css = {css`
+                    font-size: 60px;
+                    display: inline-block;
+                    animation: 1s ${spin} linear infinite;
+
+                    &:hover{
+                        animation: 1s ${spin} linear infinite reverse;
+                        text-decoration:underline;
+                    }
+                `}
+                role = "img" 
+                aria-label = "logo">😊</span>
+        </header>
+    );
+};
+
+export default NavBar;
