a355fd09b4b8dae9d4d4b03d0c3360e0bf5aa125
Akash Gohel <akash.gohel.37231@gmail.com>
2019-09-23 14:25:54 +0530

    Chnage in App,js file line 10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
 
 const App = () => {
-  const theme = useState("pink");
+  const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
       <div>
